9f313c715deab8aeef402c16627088d5d0754d7e
ahumanonline <workby@ahuman.online>
2023-02-08 13:04:49 -0500

    Rebuild TOCBot to produce checkboxes, remove TOCBot CSS

---
+++ assets/js/checkbox-filter.js
@@ -0,0 +1,20 @@
+/* eslint-env browser */
+
+/**
+ * Remove parent divs when one particular child is empty
+ * Used on procedure and index pages to remove tags from
+ * display and the generated table of contents.
+ */
+ 
+function evaluate(){
+	 var item = $(this);
+	 var relatedItem = $("#" + item.attr("data-related-item")).parent();
+	
+	 if(item.is(":checked")){
+		 $(this).closest('div.visibility-container').removeClass('hidden');
+	 }else{
+		 relatedItem.fadeOut();   
+	 }
+ }
+ 
+ $('input[type="checkbox"]').click(evaluate).each(evaluate);
