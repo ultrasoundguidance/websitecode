51d7a6b8befc4b1eaeec2d45f45315baf7cbadc0
ahumanonline <workby@ahuman.online>
2023-02-23 16:26:04 -0500

    Style TOCBox

--- assets/js/checkbox-filter.js
+++ assets/js/checkbox-filter.js
@@ -5,17 +5,10 @@
  * Used on procedure and index pages to remove tags from
  * display and the generated table of contents.
  */
- /*
-function evaluate(){
-	 var item = $(this);
-	 var relatedItem = $("#" + item.attr("data-related-item")).parent();
-	
-	 if(item.is(":checked")){
-		 $(this).closest('div.visibility-container').removeClass('hidden');
-	 }else{
-		 relatedItem.fadeOut();   
-	 }
+/*
+function uncheckAll(){
+    $('input[type="checkbox"]:checked').prop('checked',false);
  }
  
- $('input[type="checkbox"]').click(evaluate).each(evaluate);
+ document.querySelector('#deselect').addEventListener('click', uncheckAll)
  */
