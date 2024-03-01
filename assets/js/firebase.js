55375ffda859339bf89276256dbb8ea5e535c4e6
Nikki Vaughan <vnikki13@gmail.com>
2024-03-01 13:50:50 -0500

    Connect Firebase to record watched videos and create forms

---
+++ assets/js/firebase.js
@@ -0,0 +1,11 @@
+// Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyCM0FHbolc3QT-PfoLixcoeVS3JoWhheEg",
+  authDomain: "ultrasound-guidance.firebaseapp.com",
+  projectId: "ultrasound-guidance",
+  storageBucket: "ultrasound-guidance.appspot.com",
+  messagingSenderId: "947728965057",
+  appId: "1:947728965057:web:32b2bb26469a78c9762340",
+};
+firebase.initializeApp(firebaseConfig);
+const firebaseDB = firebase.firestore();
---
+++ assets/js/video-timeline.js
@@ -0,0 +1,21 @@
+const postItem = document.querySelector(".postItem")
+
+if (postItem) {
+  firebaseDB.collection("users").doc(postItem.dataset.memberEmail).get()
+    .then((doc) => {
+      if (doc.exists) {
+        const watchedVideos = doc.data().watchedVideos;
+        Object.entries(watchedVideos).forEach(([key, value]) => {
+          var element = document.getElementById(key)
+          element.style.setProperty("--progress-position", value.progressPosition)
+        });
+
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    })
+    .catch((error) => {
+      console.log("Error getting document:", error);
+    }); 
+}
