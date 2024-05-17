44d786e221da093ad0792328fee3046ccf06af1d
Nikki Vaughan <vnikki13@gmail.com>
2024-05-17 16:04:23 -0400

    Change video data schema saved to database

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -4,7 +4,7 @@ var formContainer = document.querySelector(".form");
 var submit = document.querySelector(".submit-form");
 var closeBtn = document.getElementById("closeBtn");
 var postData = document.querySelector("#post");
-var iframe = document.querySelector('iframe');
+var iframes = document.querySelectorAll('iframe');
 var formName = 'Default'
 
 // Form Rendered
@@ -38,7 +38,7 @@ window.onclick = function (event) {
     modal.style.display = "none";
   };
 };
-  
+
 if (submit) {
   submit.onclick = function (event) {
     event.preventDefault();
@@ -46,15 +46,16 @@ if (submit) {
     var answers = {}
     inputs.forEach((response) => {
       answers[response.id] = {
-          questionId: response.id,
-          textAnswers: {
-            answers: [
-              {value: response.value}
-            ]
-          }
+        questionId: response.id,
+        textAnswers: {
+          answers: [
+            { value: response.value }
+          ]
         }
-      })
+      }
+    })
 
+    // Maybe change this to each post so you answer one survey per post incase a post has two videos
     player.getVideoId().then((videoId) => {
       firebaseDB.collection("formResponses").doc(formName).set({
         responses: {
@@ -79,93 +80,109 @@ if (submit) {
 };
 
 // Vimeo
-if (postData && iframe) {
-  var player = new Vimeo.Player(iframe);
+if (postData && iframes) {
+  iframes.forEach((iframe) => {
 
-  // Set watched seconds on Vimeo Player
-  firebaseDB.collection("users").doc(postData.dataset.memberEmail).get()
-    .then((doc) => {
-      if (doc.exists) {
-        const watchedSeconds = doc.data().watchedVideos[postData.dataset.postId]['watchedSeconds']
-        player.setCurrentTime(watchedSeconds).then((seconds) => {
-          // `seconds` indicates the actual time that the player seeks to
-        }).catch((error) => {
-          switch (error.name) {
-            case 'RangeError':
-              // The time is less than 0 or greater than the video's duration
-              break;
-            default:
-              // Some other error occurred
-              break;
-          }
-        });
-      } else {
-        // doc.data() will be undefined in this case
-        console.log("Unable to get Video watched seconds: No such document");
-      }
-    })
-    .catch((error) => {
-      console.log("No document found:", error);
-    });
+    var player = new Vimeo.Player(iframe);
 
-  // Video Played
-  player.on('play', () => {
+    // Set watched seconds on Vimeo Player
     player.getVideoId().then((videoId) => {
-      firebaseDB.collection("users").doc(postData.dataset.memberEmail).set({
-        watchedVideos: {
-          [postData.dataset.postId]: {
-            postId: postData.dataset.postId,
-            videoId: videoId,
+      firebaseDB.collection("users").doc(postData.dataset.memberEmail).get()
+        .then((doc) => {
+          if (doc.exists) {
+            const watchedSeconds = doc.data().watchedVideosProgress[postData.dataset.postId][videoId]['watchedSeconds']
+            player.setCurrentTime(watchedSeconds).then(() => {
+              // `seconds` indicates the actual time that the player seeks to
+            }).catch((error) => {
+              switch (error.name) {
+                case 'RangeError':
+                  // The time is less than 0 or greater than the video's duration
+                  break;
+                default:
+                  // Some other error occurred
+                  break;
+              }
+            });
+          } else {
+            // doc.data() will be undefined in this case
+            console.log("Unable to get Video watched seconds: No such document");
           }
-        }
-      }, { merge: true })
+        })
         .catch((error) => {
-          console.error("Error adding document: ", error);
+          console.log("No document found:", error);
         });
-    });
-  });
-    
-  // Video Paused
-  player.on('pause', () => {
-    player.getCurrentTime().then((seconds) => {
-      player.getDuration().then((duration) => {
+    })
+
+
+    // Video Played
+    player.on('play', () => {
+      player.getVideoId().then((videoId) => {
+        const videoCount = document.querySelectorAll(".kg-embed-card").length
+
         firebaseDB.collection("users").doc(postData.dataset.memberEmail).set({
-          watchedVideos: {
+          watchedVideosProgress: {
             [postData.dataset.postId]: {
-              watchedSeconds: seconds - 5,
-              progressPosition: seconds / duration,
+              [videoId]: {
+                postId: postData.dataset.postId,
+                videoId: videoId,
+              },
+              videoCount: videoCount
             }
           }
         }, { merge: true })
           .catch((error) => {
-            console.error("Error updating document: ", error);
+            console.error("Error adding document: ", error);
           });
       });
     });
-  });
 
-  // Video Ended
-  player.on('ended', () => {
-    // Uncomment when ready to use forms
-    // getForm()
-    // modal.style.display = "block"
-
-    player.getDuration().then((duration) => {
-      firebaseDB.collection("users").doc(postData.dataset.memberEmail).set({
-        watchedVideos: {
-          [postData.dataset.postId]: {
-            watchedSeconds: 60,
-            progressPosition: 1,
-          }
-        }
-      }, { merge: true })
-        .then(() => {
-          player.off('pause')
-        })
-        .catch((error) => {
-          console.error("Error updating document on end event: ", error);
+    // Video Paused
+    player.on('pause', () => {
+      player.getCurrentTime().then((seconds) => {
+        player.getDuration().then((duration) => {
+          player.getVideoId().then((videoId) => {
+            firebaseDB.collection("users").doc(postData.dataset.memberEmail).set({
+              watchedVideosProgress: {
+                [postData.dataset.postId]: {
+                  [videoId]: {
+                    watchedSeconds: seconds - 5,
+                    progressPosition: seconds / duration,
+                  }
+                }
+              }
+            }, { merge: true })
+              .catch((error) => {
+                console.error("Error updating document: ", error);
+              })
+          });
         });
+      });
     });
-  });
+
+    // Video Ended
+    player.on('ended', () => {
+      // Uncomment when ready to use forms
+      // getForm()
+      // modal.style.display = "block"
+
+      player.getCurrentTime().then((seconds) => {
+        firebaseDB.collection("users").doc(postData.dataset.memberEmail).set({
+          watchedVideos: {
+            [postData.dataset.postId]: {
+              watchedSeconds: 60,
+              progressPosition: 1,
+            }
+          }
+        }, { merge: true })
+          .then(() => {
+            player.off('pause')
+          })
+          .catch((error) => {
+            console.error("Error updating document on end event: ", error);
+          });
+      });
+    });
+  })
+
 
 };
--- assets/js/video-timeline.js
+++ assets/js/video-timeline.js
@@ -1,21 +1,37 @@
-const postItem = document.querySelector(".postItem")
+window.addEventListener('DOMContentLoaded', function () {
+  const postItem = document.querySelector(".postItem")
 
-if (postItem) {
-  firebaseDB.collection("users").doc(postItem.dataset.memberEmail).get()
-    .then((doc) => {
-      if (doc.exists) {
-        const watchedVideos = doc.data().watchedVideos;
-        Object.entries(watchedVideos).forEach(([key, value]) => {
-          var element = document.getElementById(key)
-          element.style.setProperty("--progress-position", value.progressPosition)
-        });
+  if (postItem) {
+    firebaseDB.collection("users").doc(postItem.dataset.memberEmail).get()
+      .then((doc) => {
+        if (doc.exists) {
 
-      } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-      }
-    })
-    .catch((error) => {
-      console.log("Error getting document:", error);
-    });
-}
+          const watchedVideosProgress = doc.data().watchedVideosProgress;
+          let videoCount = 1
+          Object.entries(watchedVideosProgress).forEach(([key, value]) => {
+            if (value.videoCount) {
+              videoCount = value.videoCount
+            }
+            let progressPosition = 0
+
+            Object.entries(value).forEach(([videoId, value]) => {
+              // Need to divide this by how many videos there are in a post
+              if (value.progressPosition) {
+                progressPosition += value.progressPosition
+              }
+            })
+
+            var element = document.getElementById(key)
+            element.style.setProperty("--progress-position", progressPosition / videoCount)
+          });
+
+        } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+        }
+      })
+      .catch((error) => {
+        console.log("Error getting document:", error);
+      });
+  }
+})
