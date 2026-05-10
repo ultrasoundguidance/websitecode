4e8a54f2832f44532182ea1f3140598eb4be84e9
Nikki Vaughan <vnikki13@users.noreply.github.com>
2026-05-10 12:42:40 -0400

    Fix url base api

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,4 +4,4 @@
  */
 
 // API Base URL - change this to point to your API endpoint
-const API_BASE_URL = 'https://backend-947728965057.us-east1.run.app/api/v1';
+const API_BASE_URL = 'https://backend-947728965057.us-east1.run.app';
